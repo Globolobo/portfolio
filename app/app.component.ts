b046195db2bf140c0bc167038190a3f365d84fed
Sean Pichardo <sean.pichardo@nike.com>
2016-03-12 18:54:33 -0800

    Name and title working. Everything else is ommitted.

---
+++ app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <h1>{{name}}</h1>
+    <h2>{{title}}</h2>
+  `,
+    styleUrls: ['app/app.component.css'],
+    directives: [ROUTER_DIRECTIVES],
+    providers: [
+        ROUTER_PROVIDERS,
+        HeroService
+    ]
+})
+@RouteConfig([
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },
+    {
+        path: '/detail/:id',
+        name: 'HeroDetail',
+        component: HeroDetailComponent
+    },
+    {
+        path: '/heroes',
+        name: 'Heroes',
+        component: HeroesComponent
+    }
+])
+export class AppComponent {
+    name = 'Sean Pichardo';
+    title = 'Senior Web Application Developer';
+}
